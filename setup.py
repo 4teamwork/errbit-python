f91b8fa821cce78a9858dfccf8dc99e2f7a594ea
Jonas Baumann <jone@jone.ch>
2013-08-20 09:49:29 +0200

    Bootstrap package.

---
+++ setup.py
@@ -0,0 +1,46 @@
+import os
+from setuptools import setup, find_packages
+
+
+version = '1.0.0dev0'
+
+
+tests_require = [
+    ]
+
+
+setup(name='errbit',
+      version=version,
+      description='An errbit client for python',
+
+      long_description=open('README.rst').read() + '\n' + \
+          open(os.path.join('docs', 'HISTORY.txt')).read(),
+
+      classifiers=[
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+
+      keywords='errbit client python',
+      author='4teamwork GmbH',
+      author_email='mailto:info@4teamwork.ch',
+      url='https://github.com/4teamwork/errbit-python',
+
+      license='GPL2',
+      packages=find_packages(exclude=['ez_setup']),
+      include_package_data=True,
+      zip_safe=False,
+
+      install_requires=[
+        'setuptools',
+        ],
+
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
